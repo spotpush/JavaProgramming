a025b9ada03bee8778ff308540a35bcdb1b0aa16
stephen.hardy <sjhardy@btopenworld.com>
2020-03-21 18:42:25 +0000

    create inheritance class file

---
+++ Polymorphism/src/main/java/com/spotpush/polymorphism/Polymorphism.java
@@ -0,0 +1,15 @@
+/**
+Class for defining Polymorphism in Java
+*/
+/** @author Stephen Hardy @ spotpush.com  */
+package com.spotpush.polymorphism;
+
+/*Imports*/
+
+public class Polymorphism 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
