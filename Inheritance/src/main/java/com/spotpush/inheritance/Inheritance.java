7f259c789faccc4504d12b1a28c45b507c971f77
stephen.hardy <sjhardy@btopenworld.com>
2020-03-21 18:37:42 +0000

    create inheritance class file

---
+++ Inheritance/src/main/java/com/spotpush/inheritance/Inheritance.java
@@ -0,0 +1,15 @@
+/**
+class file for working with inheritance
+*/
+/** @author Stephen Hardy @ spotpush.com  */
+package com.spotpush.inheritance;
+
+/*Imports*/
+
+public class Inheritance 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
