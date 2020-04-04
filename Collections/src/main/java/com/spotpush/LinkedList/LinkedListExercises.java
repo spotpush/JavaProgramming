a0157b239ad151a59a3fb893eee18d54e87e0394
spotpush <sjhardy@btopenworld.com>
2020-04-04 22:26:44 +0100

    collections linkedlist

--- Collections/src/main/java/com/spotpush/LinkedList/LinkedListExercises.java
+++ Collections/src/main/java/com/spotpush/LinkedList/LinkedListExercises.java
@@ -6,6 +6,7 @@ Exploring the use of linked lists
 package com.spotpush.LinkedList;
 
 /*Imports*/
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
@@ -19,34 +20,38 @@ public class LinkedListExercises
         lL.add("B");
         lL.add("C");
         System.out.println(lL);
-
-        //1 - Write a Java program to append the specified element to the end 
-        //of a linked list
         
+        /**
+         * 1 - Write a Java program to append the specified element to the end 
+         */
         lL.add("F");
         System.out.println(lL);
         
-        //2 - Write a Java program to iterate through all elements in a linked list.
-        
-            //A - Classic For loop
-            System.out.println("Classic For Loop");
-            for (int i=0;i<=lL.size()-1;i++){//add -1 avoids out of bounds exception
-                String lList = lL.get(i);
-                System.out.println(lList);
-            }
-        
-            //B - enhanced for loop
-            System.out.println("Enhanced For Loop");
-            for (String lList : lL){
-                System.out.println(lList);
-            }
-            
-            //C - Iterator Method
-            System.out.println("ListIterator");
-            ListIterator iterator = lL.listIterator();//listIterator of type list
-            while(iterator.hasNext()){
-                System.out.println(iterator.next());
-            }
+        /**
+         * 2 - Write a Java program to iterate through all elements in a linked list.
+         * 
+         * A - Classic For loop
+         */
+        System.out.println("Classic For Loop");
+        for (int i=0;i<=lL.size()-1;i++){//add -1 avoids out of bounds exception
+            String lList = lL.get(i);
+            System.out.println(lList);
+        }
+        /**
+         * B - enhanced for loop
+         */
+        System.out.println("Enhanced For Loop");
+        for (String lList : lL){
+            System.out.println(lList);
+        }
+        /**
+         * C - Iterator Method
+         */    
+        System.out.println("ListIterator");
+        ListIterator iterator = lL.listIterator();//listIterator of type list
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
             
             //D - forEach lambda expression
             System.out.println("forEach lanbda expression");
@@ -65,9 +70,40 @@ public class LinkedListExercises
             }
             
         //4 - Write a Java program to iterate a linked list in reverse order.
-            System.out.println("Reverse order");
-            for(int i=lL.size();i>=0;i--){
+            System.out.println("Reverse order classic for-loop");
+            //reverse for-loop
+            for(int i=lL.size()-1;i>=0;i--){
                 System.out.println(lL.get(i));
             }
+            
+            System.out.println("Reverse order decendingIterator()");
+            Iterator itor = lL.descendingIterator();//stores a reverse copy of lL in itor
+            while (itor.hasNext()) {//prints each element forward
+                System.out.println(itor.next());
+            }
+            
+        //5 - Write a Java program to insert the specified element at the specified position in the linked list.
+            System.out.println("Add DD into the 3rd index");
+            lL.add(1,"DD"); //1 specifies the location preceding addition or "DD" 
+            System.out.println(lL);
+        
+        //6 - Write a Java program to insert elements into the linked list at the first and last position.
+            System.out.println("Add element in the 'First' and 'Last' positions");
+            lL.addFirst("First");
+            lL.addLast("Last");
+            System.out.println(lL);
+            
+            lL.offerFirst("offerFirst");
+            lL.offerLast("offerLast");
+            System.out.println(lL);
+            
+        /**
+         * Write a Java program to display the elements and their positions 
+         * in a linked list.
+         */    
+        System.out.println("Display LinkedList Index pointer and Element");
+        for (int p=0;p<lL.size();p++) {
+            System.out.println("Element Index "+p+" : "+lL.get(p));
+        }     
     }
 }
